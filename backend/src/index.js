802ccd5024c447189dc3e5e1b6d16012f9705f12
JackAnsh23 <raiansh230405@gmail.com>
2025-06-08 12:00:00 +0530

    started working and building the backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import songRoutes from "./routes/songRoutes.js";
+import albumRoutes from "./routes/albumRoutes.js";
+import statsRoutes from "./routes/statsRouts.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
 
+app.use("/api/users", userRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/songs", songRoutes);
+app.use("/api/albums", albumRoutes);
+app.use("/api/stats", statsRoutes);
+
+
 app.listen(PORT, () => {
     console.log(`server is running at ${PORT}`);
+    connectDB();
 });
