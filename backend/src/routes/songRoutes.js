bc6137f12197fe6baf23df12c396fefef7329e9c
JackAnsh23 <raiansh230405@gmail.com>
2025-06-09 12:00:00 +0530

    song controllers are added with many features backend

--- backend/src/routes/songRoutes.js
+++ backend/src/routes/songRoutes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import { getAllsongs, getFeaturedSongs, getMadeForYouSongs, getTrendingSongs } from "../controllers/songControllers.js";
+import { protectRoute, requireAdmin } from "../middlewares/authmiddlewares.js";
 
 const router = Router ();
 
-router.get("/", (req, res) => {
-    res.send("song route with GET method");
-});
+router.get("/", protectRoute, requireAdmin ,getAllsongs);
+router.get("/:featured", getFeaturedSongs );
+router.get("/:made-for-you", getMadeForYouSongs );
+router.get("/:trending", getTrendingSongs );
+
 
 export default router;
